fdbc1dfbbb495552faf3cc307ff6d29b58633e57
Kevin Yang <yangkevi@usc.edu>
2017-12-01 21:31:02 -0800

    add gapi onload listener for Google Sign-in integration

--- app/assets/javascripts/google-api.js
+++ app/assets/javascripts/google-api.js
@@ -1,7 +1,32 @@
-//TODO create promise that resolves when gapi is loaded, to init gapi.auth2
+function gapiLoaded() {
+  gapi.signin2.render('sign-in-button', {
+    'scope': 'profile email',
+    'longtitle': true,
+    'theme': 'dark',
+    'onsuccess': onSignIn,
+    'onfailure': onFailedSignIn
+  });
+
+  var auth2;
+  gapi.load('auth2', () => {
+    gapi.auth2.init({
+      client_id: '233361855229-omsegl02h7ggvurbqk4pj0g6drs496js.apps.googleusercontent.com'
+    }).then(() => {
+      auth2 = gapi.auth2.getAuthInstance();
+      if (!auth2.isSignedIn.get()) {
+        $('#sign-in-button').css('display', 'block');
+      }
+    });
+  }); 
+}
 
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
+  console.log(profile.getEmail());
+}
+
+function onFailedSignIn(error) {
+  console.log(error);
 }
 
 function signOut() {
